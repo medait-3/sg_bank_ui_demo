8be8d16d9e995501dad8affbedcfeda41714534c
meait-3 <tchikh3@@gmail.com>
2023-03-04 17:06:08 +0100

    appbar

--- lib/home.dart
+++ lib/home.dart
@@ -48,152 +48,181 @@ class _HomeState extends State<Home> {
           onPressed: () {},
         ),
       ),
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: Container(
-              alignment: Alignment.topCenter,
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Padding(
-                          padding: EdgeInsets.all(10),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(50.0),
-                            child: Image.network(
-                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnOlm4eSbJJ3l_y78tKAD1ym4PjVnGbokEhkU3vHya&s",
-                              height: 50.0,
-                              width: 50.0,
-                              fit: BoxFit.cover, //change image fill type
-                            ),
-                          )),
-                      //Ci
-                    ],
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 17, vertical: 5),
-                    child: Row(
-                      children: [
-                        Text(
-                          "COMPTE",
-                          style: TextStyle(color: Colors.white, fontSize: 15),
-                        ),
-                        Text(
-                          " CHÉQUE DIRHAMS",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 15),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 17, vertical: 2),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          Container(
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    alignment: Alignment.topCenter,
+                    child: Column(
                       children: [
                         Row(
                           children: [
-                            Visibility(
-                              visible: _passwordVisible,
-                              child: Text(
-                                "${_prix}",
+                            Padding(
+                                padding: EdgeInsets.all(10),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(50.0),
+                                  child: Image.network(
+                                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnOlm4eSbJJ3l_y78tKAD1ym4PjVnGbokEhkU3vHya&s",
+                                    height: 50.0,
+                                    width: 50.0,
+                                    fit: BoxFit.cover, //change image fill type
+                                  ),
+                                )),
+                            //Ci
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 17, vertical: 5),
+                          child: Row(
+                            children: [
+                              Text(
+                                "COMPTE",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 18),
+                              ),
+                              Text(
+                                " CHÉQUE DIRHAMS",
                                 style: TextStyle(
                                     color: Colors.white,
                                     fontWeight: FontWeight.bold,
                                     fontSize: 18),
                               ),
-                            ),
-                            Text(
-                              " DH",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 18),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                        IconButton(
-                            iconSize: 25,
-                            color: Colors.white,
-                            icon: Icon(
-                              _passwordVisible
-                                  ? Icons.visibility
-                                  : Icons.visibility_off,
-                              size: 18,
-                            ),
-                            onPressed: () {
-                              setState(() {
-                                _passwordVisible = !_passwordVisible;
-                              });
-                            }),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 17, vertical: 2),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Visibility(
+                                    visible: _passwordVisible,
+                                    child: Text(
+                                      "${_prix}",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 20),
+                                    ),
+                                  ),
+                                  Text(
+                                    " DH",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 20),
+                                  ),
+                                ],
+                              ),
+                              IconButton(
+                                  iconSize: 25,
+                                  color: Colors.white,
+                                  icon: Icon(
+                                    _passwordVisible
+                                        ? Icons.visibility
+                                        : Icons.visibility_off,
+                                    size: 18,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      _passwordVisible = !_passwordVisible;
+                                    });
+                                  }),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 17, vertical: 2),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.transit_enterexit,
+                                    color: Colors.white,
+                                    size: 20.0,
+                                  ),
+                                  Text(
+                                    "EN TRAITMENT",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                "+999,00 DH",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 17, vertical: 2),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.replay,
+                                    color: Colors.white,
+                                    size: 20.0,
+                                  ),
+                                  Text(
+                                    "MISE A JOUR",
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                              Text(
+                                "01/03/2023",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
                       ],
                     ),
+                    color: Colors.redAccent,
                   ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 17, vertical: 2),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.transit_enterexit,
-                              color: Colors.white,
-                              size: 20.0,
-                            ),
-                            Text(
-                              "EN TRAITMENT",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          "+999,00 DH",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.white),
-                        ),
-                      ],
-                    ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.white12,
                   ),
-                  Padding(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 17, vertical: 2),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Icon(
-                              Icons.replay,
-                              color: Colors.white,
-                              size: 20.0,
-                            ),
-                            Text(
-                              "MISE A JOUR",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          "01/03/2023",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-              color: Colors.redAccent,
+                ),
+              ],
             ),
           ),
-          Expanded(
-            child: Container(
-              color: Colors.white12,
+          Align(
+            alignment: Alignment.centerRight,
+            child: Card(
+              elevation: 50,
+              shadowColor: Colors.black,
+              color: Colors.white,
+              child: SizedBox(
+                width: 300,
+                height: 100,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    children: [
+                      SizedBox(width: 100, child: null),
+                    ],
+                  ), //Column
+                ), //Padding
+              ), //SizedBox
             ),
           ),
         ],
